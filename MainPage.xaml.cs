edae02b52179b734930913ca55342c010e0cb721
Ahbecano <canodabas05@gmail.com>
2025-04-07 23:52:00 +0300

    canfit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace CanFitApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStartExerciseClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Egzersiz", "Egzersize başlıyorsunuz!", "Tamam");
+        }
+
+        private void OnViewProgressClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("İlerleme", "İlerleme ekranı yakında gelecek!", "Tamam");
+        }
+    }
+}
